66fc060f5d90f44361c495cc98ea97626a0895a4
Jeremy Rodrigues <jeremy.rodrigues@therealreal.com>
2019-09-19 15:18:18 -0700

    Added requests_html to .toml.

--- build_next_event.py
+++ build_next_event.py
@@ -1,6 +1,9 @@
-import requests
+from requests_html import HTMLSession
+
+session = HTMLSession()
+
+r = session.get("https://www.sherdog.com/organizations/Ultimate-Fighting-Championship-UFC-2")
+about = r.html.find('event')
 
 
-ufc_event_url = "https://www.sherdog.com/organizations/Ultimate-Fighting-Championship-UFC-2"
-ufc_event_html = requests.get(ufc_event_url).text
 
