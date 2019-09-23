fe0cb21b1d20ee0e75173afd174da353cb4f57d8
Jeremy Rodrigues <jeremy.rodrigues@therealreal.com>
2019-09-23 10:27:49 -0700

    Account for fighters with unknown hometown.

--- build_next_event.py
+++ build_next_event.py
@@ -4,9 +4,7 @@ from tool_box import tools
 
 events_page = tools.html_session("https://www.sherdog.com/organizations/Ultimate-Fighting-Championship-UFC-2")
 next_event_url = tools.next_event_url(events_page)
-
 next_event_page = tools.html_session(next_event_url)
-
 fights = tools.fighters_on_card(next_event_page)
 
 for fight in fights:
