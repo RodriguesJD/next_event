8e6eea99a4b9d67eaad9edcce1787484feaeace3
Jeremy Rodrigues <jeremy.rodrigues@therealreal.com>
2019-09-24 09:27:13 -0700

    Basic test for month_str_to_int.

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -4,3 +4,8 @@ from tool_box import tools
 def test_html_session():
     session = tools.html_session("https://www.sherdog.com/events/UFC-Fight-Night-159-Rodriguez-vs-Stephens-76587")
     assert session.status_code == 200
+
+
+def test_month_str_to_int():
+    sep = tools.month_str_to_int("Sep")
+    assert sep == 9
